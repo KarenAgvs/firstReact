cee3546df71f679f7d80b50fd9888b1ace48098d
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-01-31 23:25:38 +0100

    Add new component

--- src/Components/Layouts/Body/Body.jsx
+++ src/Components/Layouts/Body/Body.jsx
@@ -1,8 +1,10 @@
 import { Card } from '../../UI/Card/Card.jsx';
+import {Text } from '../../UI/Text/Text.jsx';
 
 export const Body =()=>{
     return(
         <>
+        <Text/>
         <Card/>
         </>
     )
---
+++ src/Components/UI/Text/Text.jsx
@@ -0,0 +1,13 @@
+import './Text.css';
+export const Text=()=>{
+    return(
+        <>
+        <div className="AllText">
+            <h2 className="titles">¿Qué es?</h2>
+            <p>Karen Agudelo</p>
+            <h2 className="titles">¿React y su arquitectura?</h2>
+            <h2 className="titles">¿Virtual Doom?</h2>
+        </div>
+        </>
+    )
+}
